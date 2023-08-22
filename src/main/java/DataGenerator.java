41f7778aabea5bf5f1e407501828b82287659583
micsport13 <micsport13@gmail.com>
2023-08-22 08:04:29 -0600

    Adding tables into main function

--- src/main/java/DataGenerator.java
+++ src/main/java/DataGenerator.java
@@ -1,4 +1,6 @@
+import Entities.License;
 import Entities.Member;
+import Entities.Table;
 import com.github.javafaker.Faker;
 
 import java.util.ArrayList;
@@ -7,22 +9,28 @@ import java.util.List;
 public class DataGenerator {
     public static void main(String[] args) {
         Faker faker = new Faker();
-        List<Member> members = new ArrayList<>();
+        Table<Member> members = new Table<>();
+        Table<License> licenses = new Table<>();
         long start = System.nanoTime();
-        for (int i = 1; i <= 1_000_000; i++) {
+        for (int i = 1; i <= 10; i++) {
             members.add(new Member.Builder().withColumnValue("Id", i)
                                 .withColumnValue("FirstName", faker.name()
                                         .firstName())
-                                .withColumnValue("Email", faker.internet()
-                                        .emailAddress())
                                 .withColumnValue("LastName", faker.name()
                                         .lastName())
-                                .withColumnValue("Phone", faker.phoneNumber()
-                                        .phoneNumber())
+                                .withColumnValue("Email", faker.internet()
+                                        .emailAddress())
+                                .withColumnValue("Phone", faker.phoneNumber().phoneNumber())
+                                .build());
+            licenses.add(new License.Builder().withColumnValue("Id", i)
+                                .withColumnValue("Name", faker.company().name())
                                 .build());
         }
-        for (Member member : members) {
+        for (Member member : members.getEntities()) {
             System.out.println(member.toRecord());
+        }
+        for (License license : licenses.getEntities()) {
+            System.out.println(license.toRecord());
         }
         long stop = System.nanoTime();
         System.out.println("Time elapsed: " + (stop - start) / 1_000_000 + " milliseconds");
