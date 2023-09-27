7b2e3cb50599814376c00f4c78c812b8d0e554b4
micsport13 <micsport13@gmail.com>
2023-09-26 22:42:32 -0600

    Making main method more generic to create tables based on CLI input

--- src/main/java/Data/Validators/TableValidators/UniqueValidator.java
+++ src/main/java/Data/Validators/TableValidators/UniqueValidator.java
@@ -37,3 +37,4 @@ public class UniqueValidator implements TableValidator {
         return "Unique";
     }
 }
+
--- src/main/java/DataGenerator.java
+++ src/main/java/DataGenerator.java
@@ -7,7 +7,10 @@ import Data.Validators.TableValidators.PrimaryKeyValidator;
 import Data.Validators.TableValidators.UniqueValidator;
 import com.github.javafaker.Faker;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Random;
+import java.util.Scanner;
 
 /**
  * The type Data generator.
@@ -19,36 +22,80 @@ public class DataGenerator {
      * @param args the input arguments
      */
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
         Faker faker = new Faker();
-        Schema schema = new Schema(
-         new Column<>("id", Integer.class),
-        new Column<>("first_name", String.class, new NotNullValidator()),
-        new Column<>("last_name", String.class, new NotNullValidator()),
-        new Column<>("email", String.class, new NotNullValidator()),
-        new Column<>("is_admin", Boolean.class, new NotNullValidator())
-        );
-        Table members = new Table("Members",schema);
-        members.addTableConstraint(schema.getColumn("id"), new PrimaryKeyValidator());
-        members.addTableConstraint(schema.getColumn("id"), new UniqueValidator()); // FIXME: This overwrites existing validators.
-        members.addTableConstraint(schema.getColumn("email"), new UniqueValidator());
-        long startTime = System.nanoTime();
-        for (int i = 0; i < 100_000; i++) {
-            Entity entity = new Entity.Builder(members.getSchema().getColumns().toArray(new Column[0]))
-                    .withColumnValue("id", i)
-                    .withColumnValue("first_name", faker.name()
-                            .firstName())
-                    .withColumnValue("last_name", faker.name()
-                            .lastName())
-                    .withColumnValue("email", faker.internet()
-                            .emailAddress()+i)
-                    .withColumnValue("is_admin",true)
-                    .build();
-            members.add(entity);
+        List<Column<?>> columns = new ArrayList<>();
+        Table table = null;
+        boolean loop = true;
+        while (loop) {
+            printOptions();
+
+            int option = scanner.nextInt();
+            switch (option) {
+                case 1:
+                    columns.add(createColumn(scanner));
+                    break;
+                case 2:
+                    Schema schema = createSchema(columns);
+                    System.out.print("Enter table name: ");
+                    String tableName = scanner.next();
+                    table = new Table(tableName, schema);
+                    break;
+                case 3:
+                    System.out.print("Enter number of entities to generate: ");
+                    int numEntities = scanner.nextInt();
+                    generateData(table, faker, numEntities);
+                    break;
+                case 4:
+                    loop = false;
+            }
+
         }
-        long endTime = System.nanoTime();
-        //System.out.println(members);
-        System.out.println(members.getSchema().toString());
-        System.out.println("Time taken: " + -(startTime - endTime) / 1_000_000 + "ms");
+        scanner.close();
         //System.out.println(members.printTable());
+    }
+
+    public static void printOptions() {
+        System.out.println("1. Create column");
+        System.out.println("2. Create table");
+        System.out.println("3. Generate data");
+        System.out.println("4. Exit");
+    }
+    public static Column<?> createColumn(Scanner scanner) {
+        System.out.print("Enter column name: ");
+        String columnName = scanner.next();
+
+        System.out.print("Enter data type (e.g., Integer, String, Boolean): ");
+        String dataType = scanner.next();
+
+        // Assuming you have a method to convert String data type to Class type
+        Class<?> columnType = getTypeFromClassname(dataType);
+
+        return new Column<>(columnName, columnType);
+    }
+    private static Schema createSchema(List<Column<?>> columns) {
+        return new Schema(columns.toArray(new Column[0]));
+    }
+    private static void generateData(Table table, Faker faker, int numEntities) {
+        for (int i; i<numEntities;i++) {
+            Entity entity = new Entity.Builder(table.getSchema().getColumns().toArray(new Column[0]))
+                    .withColumnValue("id", i)
+                    .build();
+            table.add(entity);
+        }
+        System.out.println(table.printTable());
+    }
+    private static Class<?> getTypeFromClassname(String className) {
+        switch (className) {
+            case "Integer":
+                return Integer.class;
+            case "String":
+                return String.class;
+            case "Boolean":
+                return Boolean.class;
+            // Add other data types if needed
+            default:
+                throw new IllegalArgumentException("Unknown data type class: " + className);
+        }
     }
 }
